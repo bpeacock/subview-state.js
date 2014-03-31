f0f400857fbc64ada6784015b27bb9728f1fc6fa
Brian Peacock <bpeacock@fastfig.com>
2014-03-30 22:36:49 -0400

    Updated website

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 subview-state.js
 ================
 
-A [subview.js](https://github.com/bpeacock/subview.js) extension that adds a state model to your subview that can be bound to and is reflected by CSS classes applied to the `$wrapper`. This is very useful for UI state changes such as hiding and showing a side bar.
+A [subview.js](http://subviewjs.com) extension that adds a state model to your subview that can be bound to and is reflected by CSS classes applied to the `$wrapper`. This is very useful for UI state changes such as hiding and showing a side bar.
 
 Usage
 -----
--- test/test.js
+++ test/test.js
@@ -2,16 +2,61 @@ var sinon = require('sinon'),
     module = window.module,
     sandbox;
 
-module("onClick", {
+var testView;
+module("State", {
     setup: function() {
-        sandbox         = sinon.sandbox.create();
-
+        sandbox = sinon.sandbox.create();
+        testView = testSubview.spawn();
     },
     teardown: function() {
-        
+
     }
 });
 
-test('sample', function() {
-    ok(true);
+test("#set", function() {
+    testView.state.set('foo', 'bar');
+
+    ok(testView.$wrapper.hasClass('state-foo-bar'), "Class added");
+    equal(testView.state.data.foo, 'bar', "Attribute set");
+});
+
+test("#get", function() {
+    testView.state.set('foo', 'bar');
+    equal(testView.state.get('foo'), 'bar', "get works");
+});
+
+test("#remove", function() {
+    testView.state.set('foo', 'bar');
+    testView.state.remove('foo');
+
+    equal(testView.state.get('foo'), undefined, "removed foo");
+    ok(!testView.$wrapper.hasClass('state-foo-bar'), "class removed");
+});
+
+test("#bind", function() {
+    var callback = sinon.spy();
+    testView.state.bind('foo', callback);
+    testView.state.set('foo', 'bar');
+
+    ok(callback.called, "Callback called");
+    ok(callback.calledWith('bar'), "Correct argument passed to callback");
+});
+
+test("#unbind", function() {
+    var callback = sinon.spy();
+    testView.state.bind('foo', callback);
+    testView.state.unbind('foo');
+    testView.state.set('foo', 'bar');
+
+    ok(!callback.called, "Callback didn't fire.");
+});
+
+test("#trigger", function() {
+    var callback = sinon.spy();
+    testView.state.set('foo', 'bar');
+    testView.state.bind('foo', callback);
+    testView.state.trigger('foo');
+
+    ok(callback.called, "Callback called");
+    ok(callback.calledWith('bar'), "Correct argument passed to callback");
 });
